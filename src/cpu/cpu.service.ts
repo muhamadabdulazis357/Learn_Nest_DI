a69292c8942e57400f9f2c7bf93def4dce8e7aae
muhamadabdulazis357 <muhamadabdulazis357@gmail.com>
2026-02-24 16:51:13 +0700

    fix it

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -6,7 +6,7 @@ export class CpuService {
   constructor(private powerService: PowerService) {}
 
   compute(a: number, b: number) {
-    console.log('CPU membutuhkan 10 watt untuk menghitung');
+    console.log('CPU needs 10 watts to compute');
     this.powerService.supplyPower(10, 'CPU');
     return a + b;
   }
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -6,8 +6,8 @@ export class DiskService {
   constructor(private powerService: PowerService) {}
 
   getData() {
-    console.log('Disk membutuhkan daya 20 watt untuk menampilkan data');
+    console.log('Disk needs 20 watts to display data');
     this.powerService.supplyPower(20, 'Disk');
-    return 'data yang diambil dari disk';
+    return 'data retrieved from disk';
   }
 }
